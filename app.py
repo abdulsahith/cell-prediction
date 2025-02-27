e9c6fcf0f4eff4f91cb33cbbfca3171b74381082
ABDUL SAHITH M <abdulsahith4@gmail.com>
2025-02-27 22:28:51 +0530

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from tensorflow.keras.models import load_model
 import numpy as np
 import joblib
 from flask_cors import CORS
-
+import os
+from dotenv import load_dotenv
 app = Flask(__name__)
 CORS(app)
 
