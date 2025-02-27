02d4aa2ebd84bf905ae385cf4fbee7e2062cbc13
ABDUL SAHITH M <abdulsahith4@gmail.com>
2025-02-27 11:35:54 +0530

    app.py

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def predict():
         return jsonify({"error": f"Prediction failed: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run()
